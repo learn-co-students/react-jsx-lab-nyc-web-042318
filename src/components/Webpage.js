8271544a3f51cd5ce313defd62af01c09b2f67b1
srajeev91 <github email address>
2018-06-25 18:22:57 -0400

    Done.

---
+++ src/components/Webpage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import FillerText from './FillerText.js'
+
+const Webpage = () => {
+  return (
+    <div>
+      <p>The world's coolest webpage</p>
+      <FillerText />
+      <FillerText />
+    </div>
+  );
+}
+
+export default Webpage;
